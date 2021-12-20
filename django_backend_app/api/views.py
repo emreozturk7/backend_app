09384f82e92256b85e9d9e983df2c701b2a11087
emreozturk <emre_oztrk_99@hotmail.com>
2021-12-20 18:54:42 +0300

    update

--- django_backend_app/api/views.py
+++ django_backend_app/api/views.py
@@ -1,5 +1,9 @@
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         {
@@ -33,4 +37,4 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes )
